398167a943dcd52d1e8512376e98156c1764f081
Piotrek <leereek@gmail.com>
2017-12-05 12:08:58 +0100

    Fixed DataBase dispose

--- src/DataAccessLayer/DataBase.java
+++ src/DataAccessLayer/DataBase.java
@@ -14,100 +14,83 @@ public class DataBase {
 	private ItemRepository itemRepository;
 	private BattleFieldHistoryRepository battleFieldHistoryRepository;
 	private StatisticRepository statisticRepository;
-	
+
 	private static StandardServiceRegistry registry;
 	private static SessionFactory sessionFactory;
-	
-	private Session session;
-	
-	  public static SessionFactory getSessionFactory() {
-	    if (sessionFactory == null) {
-	      try {
-	    	  registry = new StandardServiceRegistryBuilder()
-	  	            .configure("hibernate.cfg.xml")
-	  	            .build();
-	  	
-	        // Create MetadataSources
-	  	    MetadataSources sources = new MetadataSources(registry)
-	  	    		
-	  	    		.addAnnotatedClass(Statistic.class)
-	  	    		.addAnnotatedClass(StatisticType.class)
-	  	    	
-	  	    		.addAnnotatedClass(PlayerItem.class)
-	  	    		.addAnnotatedClass(Item.class)
-	  	    		
-	  	    		
-	  	    		.addAnnotatedClass(AttackItem.class)
-	  	    		
-	  	    		.addAnnotatedClass(Sword.class)
-	  	    		.addAnnotatedClass(Wand.class)
-	  	    		.addAnnotatedClass(Bow.class)
-	  	    		.addAnnotatedClass(Ammunition.class)
-	  	    		
-	  	    		.addAnnotatedClass(DefensiveItem.class)
-	  	    		.addAnnotatedClass(Shield.class)
-	    			.addAnnotatedClass(Armor.class)
-	    			
-	  	    		.addAnnotatedClass(Useabletem.class)
-	  	    		.addAnnotatedClass(HealingPotion.class)
-	  	    		
-	  	    		.addAnnotatedClass(AppUser.class)
-	  	    		.addAnnotatedClass(Player.class)
-	  	    		.addAnnotatedClass(Admin.class)
-	  	    		.addAnnotatedClass(Mage.class)
-	  	    		.addAnnotatedClass(Warrior.class)
-	  	    		
-	  	    		
-	  	    		.addAnnotatedClass(BattlefieldActionsHistory.class)
-	  	    		.addAnnotatedClass(BattlefieldHistory.class);
-	  	    		
-	  	    		
-	  	
-	  	    // Create Metadata
-	  	    Metadata metadata = sources.getMetadataBuilder().build();
-	  	
-	  	    // Create SessionFactory
-	  	    sessionFactory = metadata.getSessionFactoryBuilder().build();
 
-	      } catch (Exception e) {
-	        System.out.println("SessionFactory creation failed " + e.getMessage());
-	        if (registry != null) {
-	          StandardServiceRegistryBuilder.destroy(registry);
-	        }
-	      }
-	    }
-	    return sessionFactory;
-	  }
+	public static SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			try {
+				registry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
 
-	  public static void shutdown() {
-	    if (registry != null) {
-	      StandardServiceRegistryBuilder.destroy(registry);
-	    }
-	  }
-	  
-	  public void Dispose() {
-		  session.close();
-	  }
-	
-    public DataBase(RepositoryFactory repoFactory) {
-    	SessionFactory sessionFactory = getSessionFactory();
-		session = sessionFactory.openSession();
-    		
-    	this.accountRepository = repoFactory.CreateAccountRepository(sessionFactory);
-    	this.itemRepository = repoFactory.CreateItemRepository(sessionFactory);
-    	this.battleFieldHistoryRepository = repoFactory.CreateBattleFieldHistoryRepository(sessionFactory);
-    	this.statisticRepository = repoFactory.CreateStatisticRepository(sessionFactory);
-    }
-    
+				// Create MetadataSources
+				MetadataSources sources = new MetadataSources(registry)
+
+						.addAnnotatedClass(Statistic.class).addAnnotatedClass(StatisticType.class)
+
+						.addAnnotatedClass(PlayerItem.class).addAnnotatedClass(Item.class)
+
+						.addAnnotatedClass(AttackItem.class)
+
+						.addAnnotatedClass(Sword.class).addAnnotatedClass(Wand.class).addAnnotatedClass(Bow.class)
+						.addAnnotatedClass(Ammunition.class)
+
+						.addAnnotatedClass(DefensiveItem.class).addAnnotatedClass(Shield.class)
+						.addAnnotatedClass(Armor.class)
+
+						.addAnnotatedClass(Useabletem.class).addAnnotatedClass(HealingPotion.class)
+
+						.addAnnotatedClass(AppUser.class).addAnnotatedClass(Player.class).addAnnotatedClass(Admin.class)
+						.addAnnotatedClass(Mage.class).addAnnotatedClass(Warrior.class)
+
+						.addAnnotatedClass(BattlefieldActionsHistory.class).addAnnotatedClass(BattlefieldHistory.class);
+
+				// Create Metadata
+				Metadata metadata = sources.getMetadataBuilder().build();
+
+				// Create SessionFactory
+				sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+			} catch (Exception e) {
+				System.out.println("SessionFactory creation failed " + e.getMessage());
+				if (registry != null) {
+					StandardServiceRegistryBuilder.destroy(registry);
+				}
+			}
+		}
+		return sessionFactory;
+	}
+
+	public static void Dispose() {
+		if (sessionFactory != null)
+			sessionFactory.close();
+
+		if (registry != null) {
+			StandardServiceRegistryBuilder.destroy(registry);
+		}
+	}
+
+	public DataBase(RepositoryFactory repoFactory) {
+		SessionFactory sessionFactory = getSessionFactory();
+
+		this.accountRepository = repoFactory.CreateAccountRepository(sessionFactory);
+		this.itemRepository = repoFactory.CreateItemRepository(sessionFactory);
+		this.battleFieldHistoryRepository = repoFactory.CreateBattleFieldHistoryRepository(sessionFactory);
+		this.statisticRepository = repoFactory.CreateStatisticRepository(sessionFactory);
+	}
+
 	public AccountRepository getAccountRepository() {
 		return accountRepository;
 	}
+
 	public ItemRepository getItemRepository() {
 		return itemRepository;
 	}
+
 	public BattleFieldHistoryRepository getBattleFieldHistoryRepository() {
 		return battleFieldHistoryRepository;
 	}
+
 	public StatisticRepository getStatisticRepository() {
 		return statisticRepository;
 	}
