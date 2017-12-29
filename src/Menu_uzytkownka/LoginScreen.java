9fe1c41122110f2f35c9209438c1b2a0203a594f
Alexey Kostiukov <kostiukov3@gmail.com>
2017-12-29 22:08:11 +0100

    comments

--- src/Menu_uzytkownka/LoginScreen.java
+++ src/Menu_uzytkownka/LoginScreen.java
@@ -8,6 +8,17 @@ public class LoginScreen {
 		this.dataBase = dataBase;
 	}
 	public void register(String login, String password) {
+		// TODO sprawdzenie w bazie czy istnieje taki player
+		// jezeli nie, to dodanie do bazy
 		
+		// TODO gui wybor classy
+		// w zaleznosci od wybranej classy dodajemy do ekwipunku
+		// przedmity z bazy
+		
+		// mozna tu odrazu zaladowac postac z bazy jezeli taki player 
+		// istnieje w bazie lub to jest Admin
+		
+		
+		// na koncu tworzenie AppUsera
 	}
 }
